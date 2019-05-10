7d458871f721b202493a5ae6dd1c44556de5c033
David Teller <D.O.Teller@gmail.com>
2019-05-10 12:15:33 +0200

    Travis: installing realpath

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@
 set -e
 
 # Integration test for round-tripping a file.
-# Note: On MacOS X, you need to install GNU coreutils (package `coreutils`).
+# Note:
+# - On MacOS X, you need to install GNU coreutils (package `coreutils`).
+# - On Ubuntu, you need to install realpath (package `realpath`).
 
 prog_dir=$(dirname $(realpath $0))
 
